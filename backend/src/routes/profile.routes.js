ed549921138061ea2d3c7b3170ded530d730304f
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2026-02-07 12:12:42 +0530

    feat: add home content route and reorganize profile route imports

--- backend/src/routes/profile.routes.js
+++ backend/src/routes/profile.routes.js
@@ -1,9 +1,13 @@
-import { submitAnswersAndGenerateProfile } from "../controllers/profile.controllers.js";
+import {
+  submitAnswersAndGenerateProfile,
+  homePageContent,
+} from "../controllers/profile.controllers.js";
 import { Router } from "express";
 import { verifyjwt } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
-router.post("/submit-answers", verifyjwt, submitAnswersAndGenerateProfile); 
+router.post("/submit-answers", verifyjwt, submitAnswersAndGenerateProfile);
+router.get("/home-content", verifyjwt, homePageContent);
 
 export default router;
