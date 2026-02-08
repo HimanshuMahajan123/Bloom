3bf0c8cb1c036bb1652e455948d6cc1ff74f521b
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2026-02-08 16:27:15 +0530

    change cookie settings for production

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -114,6 +114,8 @@ const verifyEmail = asyncHandler(async (req, res) => {
   const jwtToken = generateJWT(updatedUser);
   const options = {
     httpOnly: true,
+    secure: true,
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   };
   res
